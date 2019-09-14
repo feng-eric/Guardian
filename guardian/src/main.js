972e36200f563e8feb769db23860c10aa989c8b2
Eric Feng <ericfeng610@gmail.com>
2019-09-14 01:12:01 -0400

    added firebase configuation

---
+++ guardian/src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import { firestorePlugin } from 'vuefire'
+import App from './App.vue'
+
+import firebase from 'firebase'
+
+Vue.config.productionTip = false
+
+Vue.use(firestorePlugin)
+
+let firebaseConfig = {
+  apiKey: "AIzaSyAr4XfjXnQ0MDKF8ItJF5g0_sfOJym5QdE",
+  authDomain: "guardian-8888.firebaseapp.com",
+  databaseURL: "https://guardian-8888.firebaseio.com",
+  projectId: "guardian-8888",
+  storageBucket: "",
+  messagingSenderId: "479861968929",
+  appId: "1:479861968929:web:e7f9b6edbc21d8f1941360"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export var database = firebase.database();
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
